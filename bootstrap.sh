18ab361a56f1ce948823922a9ca9349b1725d096
akbarmohammadi70 <akbar.mohammadi70@yahoo.com>
2022-01-01 12:27:49 +0000

    add many config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 mkdir ./a2billing 
 #clone a2billing project
-git clone https://github.com/Star2Billing/a2billing.git ./a2billing
+git clone https://github.com/akbarmohammadi70/asterisk_docker.git ./a2billing
 #restore Database Mysql
 docker exec -i mysql cat /database/a2billing-createdb-user.sql | /usr/bin/mysql -u a2billinguser --password=a2billing mya2billing
 
